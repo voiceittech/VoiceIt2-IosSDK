684ffc9f9e4c83d1aa07139eb24cad7aade8798c
Armaan Bindra <armaan.bindra@voiceit-tech.com>
2017-10-12 19:09:13 -0500

    Removed Stuff From Example

--- Example/VoiceItApi2IosSDK/VoiceItViewController.m
+++ Example/VoiceItApi2IosSDK/VoiceItViewController.m
@@ -14,26 +14,6 @@
 
 @implementation VoiceItViewController
 
-- (IBAction)startClicked:(id)sender {
-
-    NSMutableDictionary * styles = [[NSMutableDictionary alloc] init];
-    [styles setObject:@"#FE0000" forKey:@"kThemeColor"];
-    [styles setObject:@"monochrome" forKey:@"kIconStyle"];
-    
-    _myVoiceIt = [[VoiceItAPITwo alloc] init:self apiKey:@"key_4306d632525748ac987cc0385ceff339" apiToken:@"tok_c910f4047d1d425980c856bb3ca8fa5b" styles: styles];
-    [_myVoiceIt encapsulatedVideoEnrollUser:@"usr_8c69320d0bdd42549f69d3d0d53ae824" contentLanguage:@"en-US" voicePrintPhrase:@"never forget tomorrow is a new day" userEnrollmentsCancelled:^{
-
-    } userEnrollmentsPassed:^{
-
-    }];
-//    [_myVoiceIt encapsulatedVideoVerification:@"usr_8c69320d0bdd42549f69d3d0d53ae824" contentLanguage:@"en-US" voicePrintPhrase:@"never forget tomorrow is a new day" userVerificationCancelled:^{
-//    } userVerificationSuccessful:^(float a, float b, NSString * result){
-//
-//    } userVerificationFailed:^(float a, float b, NSString * result){
-//
-//    }];
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
