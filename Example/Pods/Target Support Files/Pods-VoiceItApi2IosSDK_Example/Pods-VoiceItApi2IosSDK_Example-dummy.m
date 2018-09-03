33d9795009585c52b35699cc2d720abc4742e199
Armaan Bindra <armaan@voiceit.io>
2018-09-03 13:53:04 -0500

    remove Example/Podfile.lock and update .gitignore

--- Example/Pods/Target Support Files/Pods-VoiceItApi2IosSDK_Example/Pods-VoiceItApi2IosSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VoiceItApi2IosSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_VoiceItApi2IosSDK_Example
-@end
--- Example/Pods/Target Support Files/VoiceItApi2IosSDK/VoiceItApi2IosSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VoiceItApi2IosSDK : NSObject
-@end
-@implementation PodsDummy_VoiceItApi2IosSDK
-@end
