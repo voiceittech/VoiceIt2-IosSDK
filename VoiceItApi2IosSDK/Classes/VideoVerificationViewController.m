7425d07984f15ef9ac790950888f71afa5c98a6c
akshay247-voiceit <akshay@voiceit.io>
2020-10-26 16:28:44 -0400

    IOS-22 video verification response handling and content language passed correctly

--- VoiceItApi2IosSDK/Classes/VideoVerificationViewController.m
+++ VoiceItApi2IosSDK/Classes/VideoVerificationViewController.m
@@ -98,9 +98,14 @@
     self.result = result;
     
     if(!self.success && retry){
-        [self.myVoiceIt videoVerificationWithLiveness:self.lcoId userId: self.userToVerifyUserId contentLanguage:self.contentLanguage videoPath:self.savedVideoPath phrase:self.thePhrase callback:^(NSString * result) {
-            [self handleLivenessResponse: result];
-        }];
+        [self removeLoading];
+        [Utilities deleteFile:self.audioPath];
+        [Utilities deleteFile:self.videoPath];
+        [Utilities deleteFile:self.savedVideoPath];
+        [self playSound:self.audioPromptType];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3.0 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+            [self startVerificationProcess];
+        });
     }
     
     if(!self.success && !retry){
@@ -111,6 +116,7 @@
         });
         [Utilities deleteFile:self.audioPath];
         [Utilities deleteFile:self.videoPath];
+        [Utilities deleteFile:self.savedVideoPath];
         [self playSound:self.audioPromptType];
     }
     
@@ -122,6 +128,7 @@
         });
         [Utilities deleteFile:self.audioPath];
         [Utilities deleteFile:self.videoPath];
+        [Utilities deleteFile:self.savedVideoPath];
         [self playSound:self.audioPromptType];
     }
 }
@@ -130,7 +137,7 @@
     if(!self.doLivenessDetection) return;
     
     [[self myVoiceIt] getLivenessID:self.userToVerifyUserId countryCode:
-     @"en-US" callback:^(NSString *response) {
+     self.contentLanguage callback:^(NSString *response) {
         
         NSDictionary *data = [Utilities getJSONObject:response];
         self.lcoStrings = [data valueForKey:@"lcoStrings"];
@@ -283,40 +290,7 @@
         });
         NSBundle * podBundle = [NSBundle bundleForClass: self.classForCoder];
         NSURL * bundleURL = [[podBundle resourceURL] URLByAppendingPathComponent:@"VoiceItApi2IosSDK.bundle"];
-        NSString* soundFilePath;
-        if([lco isEqualToString:@"FACE_LEFT"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/FACE_LEFT.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"FACE_RIGHT"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/FACE_RIGHT.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"SMILE"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/SMILE.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"FACE_NEUTRAL"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/FACE_NEUTRAL.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"FACE_TILT_RIGHT"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/FACE_TILT_RIGHT.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"FACE_TILT_LEFT"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/FACE_TILT_LEFT.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"FACE_UP"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/FACE_UP.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"FACE_DOWN"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/FACE_DOWN.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"LIVENESS_FAILED"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/LIVENESS_FAILED.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"LIVENESS_SUCCESS"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/LIVENESS_SUCCESS.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
-        else if([lco isEqualToString:@"LIVENESS_TRY_AGAIN"]){
-            soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/LIVENESS_TRY_AGAIN.wav",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage];
-        }
+        NSString* soundFilePath = [NSString stringWithFormat:@"%@/wav/%@/%@",[[[NSBundle alloc] initWithURL:bundleURL] resourcePath],self.contentLanguage, lco];
         NSURL *soundFileURL = [NSURL fileURLWithPath:soundFilePath];
         NSError *error;
         AVAudioSession *session = [AVAudioSession sharedInstance];
@@ -328,7 +302,7 @@
 }
 
 -(void) illuminateCircles:(NSString*) lcoSignal{
-    [self playSound:lcoSignal];
+    [self playSound: [NSString stringWithFormat:@"%@.wav",lcoSignal]];
     if ([lcoSignal isEqualToString:@"FACE_UP"]) {
         self.progressCircle.path = [UIBezierPath bezierPathWithArcCenter: self.cameraCenterPoint radius:(self.backgroundWidthHeight / 2) startAngle: 1.2 * M_PI endAngle: 1.8 * M_PI clockwise:YES].CGPath;
         self.progressCircle.drawsAsynchronously = YES;
