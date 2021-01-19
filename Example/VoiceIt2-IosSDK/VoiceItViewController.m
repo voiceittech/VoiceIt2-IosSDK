8fca453d7ad06b253d793901801a116b104c8c80
noelgrover <>
2021-01-19 17:40:17 -0600

    IOS-29 Verified .../v1 endpoint and force light mode inside of framework

--- Example/VoiceIt2-IosSDK/VoiceItViewController.m
+++ Example/VoiceIt2-IosSDK/VoiceItViewController.m
@@ -17,12 +17,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.API_KEY = @"key_1df99ca38b97488499e1fe9db9a55e6c";
-    self.API_TOKEN = @"tok_3d43dcd14f6349ee93247b4a3eb2adc4";
-    self.TEST_USER_ID_ONE = @"usr_69a5cea13e434ef9b5f0820915374d6b";
-    self.TEST_USER_ID_TWO = @"usr_0987ad4aaec447dea88beb57b53addb9";
+    self.API_KEY = @"API_KEY_HERE";
+    self.API_TOKEN = @"API_TOKEN_HERE";
+    self.TEST_USER_ID_ONE = @"USER_ID_HERE";
+    self.TEST_USER_ID_TWO = @"USER_ID_HERE";
     self.TEST_USER_ID = self.TEST_USER_ID_ONE;
-    self.TEST_GROUP_ID = @"grp_2a0b47a8174b44f89587aebd95086ae2";
+    self.TEST_GROUP_ID = @"GROUP_ID_HERE";
     self.TEST_PHRASE = @"never forget tomorrow is a new day";
     self.TEST_CONTENT_LANGUAGE = @"en-US";
     
